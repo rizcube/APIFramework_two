25a3af3516b3037063895d346d37e0c227834ba0
rizcube <rizcubeservices@gmail.com>
2020-05-19 13:43:14 +0100

    test

---
+++ src/main/java/bdd/APIFramework_two/App.java
@@ -0,0 +1,13 @@
+package bdd.APIFramework_two;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
